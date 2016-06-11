edc2f5990f1d6688ae8f0d417b26ae8b559f002c
Anne Johnson <annekjohnson23@yahoo.com>
2016-06-11 12:39:09 -0400

    Refactor some more

--- lib/taxon_search.ex
+++ lib/taxon_search.ex
@@ -9,9 +9,16 @@ defmodule TaxonSearch do
                      else
                        filtered_results
                      end
-    Enum.map(results_to_use, fn(result) -> result["species"] end)
+    get_unique_species_names(results_to_use)
+  end
+
+  defp get_unique_species_names(results) do
+    Enum.map(results, &get_species_name/1)
     |> Enum.uniq
   end
+
+  defp get_species_name(%{"species" => species}), do: species
+  defp get_species_name(_), do: nil
 
   defp filter_by_common_name(results, common_name) do
     Enum.filter results, fn(result) ->
@@ -19,15 +26,21 @@ defmodule TaxonSearch do
     end
   end
 
-  defp matching_common_name?(result, common_name) do
-    name_maps = result["vernacularNames"] || []
-    common_name_regexes = Utils.get_token_regexes(common_name)
-    matching_english_name? = fn(name_map) ->
-      name_map["language"] == "eng" &&
-        Utils.all_regexes_match?(common_name_regexes, name_map["vernacularName"])
-    end
+  defp matching_english_name?(%{"language" => "eng", "vernacularName" => vernName}, name) do
+    Utils.get_token_regexes(name)
+    |> Utils.all_regexes_match?(vernName)
+  end
+  defp matching_english_name?(_, _) do
+    false
+  end
 
-    Enum.any?(name_maps, matching_english_name?)
+  defp matching_common_name?(%{"vernacularNames": name_maps}, common_name) do
+    Enum.any?(name_maps, fn(name_map) ->
+      matching_english_name?(name_map, common_name)
+    end)
+  end
+  defp matching_common_name?(_, _) do
+    false
   end
 
   defp get_results(common_name, http_module) do
@@ -38,7 +51,8 @@ defmodule TaxonSearch do
   end
 
   defp make_species_search_request(query, http_module) do
-    query_str = Map.merge(search_params, %{q: query}) |> URI.encode_query
+    query_str = Map.merge(search_params, %{q: query})
+                |> URI.encode_query
     request_url = api_url <> "/species/search?" <> query_str
     http_module.get(request_url, [timeout: timeout_milliseconds])
   end
