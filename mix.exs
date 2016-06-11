108301502d94072c075157635feafabd5a3f1d15
Anne Johnson <annekjohnson23@yahoo.com>
2016-06-11 14:03:38 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,27 +4,32 @@ defmodule TaxonSearch.Mixfile do
   def project do
     [app: :taxon_search,
      version: "0.0.1",
+     description: description,
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     package: package]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [
-      applications: [:logger, :httpotion]
-    ]
+    [applications: [:logger, :httpotion]]
+  end
+
+  defp description do
+    "TaxonSearch is a tool for looking up species names in Elixir."
+  end
+
+  defp package do
+    [licenses: ["MIT"],
+     maintainers: ["annecodes@gmail.com"],
+     links: %{"GitHub" => "https://github.com/annejohnson/taxon_search"}]
   end
 
   defp deps do
-    [
-      {:httpotion, "~> 2.2.0"},
-      {:poison, "~> 2.0"},
-      {:earmark, "~> 0.1", only: :dev},
-      {:ex_doc, "~> 0.11", only: :dev}
-    ]
+    [{:httpotion, "~> 2.2.0"},
+     {:poison, "~> 2.0"},
+     {:earmark, "~> 0.1", only: :dev},
+     {:ex_doc, "~> 0.11", only: :dev}]
   end
 end
