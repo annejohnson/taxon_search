d03b99ab47a85483f7727b94c528d6cf453cc6c8
Anne Johnson <annekjohnson23@yahoo.com>
2016-06-11 12:50:20 -0400

    Move code related to hitting API into a separate module

--- lib/taxon_search.ex
+++ lib/taxon_search.ex
@@ -1,8 +1,8 @@
 defmodule TaxonSearch do
-  alias TaxonSearch.StringUtils
+  alias TaxonSearch.{StringUtils, Api}
 
-  def get_species_names(common_name, http_module \\ HTTPotion) do
-    all_results = get_results(common_name, http_module)
+  def get_species_names(common_name) do
+    all_results = Api.get_results(common_name)
     filtered_results = filter_by_common_name(all_results, common_name)
     results_to_use = if Enum.empty?(filtered_results) do
                        all_results
@@ -41,31 +41,5 @@ defmodule TaxonSearch do
   end
   defp matching_common_name?(_, _) do
     false
-  end
-
-  defp get_results(common_name, http_module) do
-    response = make_species_search_request(common_name, http_module)
-    parsed_response_body = Poison.decode!(response.body)
-
-    parsed_response_body["results"]
-  end
-
-  defp make_species_search_request(query, http_module) do
-    query_str = Map.merge(search_params, %{q: query})
-                |> URI.encode_query
-    request_url = api_url <> "/species/search?" <> query_str
-    http_module.get(request_url, [timeout: timeout_milliseconds])
-  end
-
-  defp api_url do
-    "http://api.gbif.org/v1"
-  end
-
-  defp search_params do
-    %{limit: 50, nameType: "SCIENTIFIC", rank: "SPECIES", status: "ACCEPTED"}
-  end
-
-  defp timeout_milliseconds do
-    10_000
   end
 end
---
+++ lib/taxon_search/api.ex
@@ -0,0 +1,27 @@
+defmodule TaxonSearch.Api do
+  def get_results(common_name, http_module \\ HTTPotion) do
+    response = make_species_search_request(common_name, http_module)
+    parsed_response_body = Poison.decode!(response.body)
+
+    parsed_response_body["results"]
+  end
+
+  defp make_species_search_request(query, http_module) do
+    query_str = Map.merge(search_params, %{q: query})
+                |> URI.encode_query
+    request_url = api_url <> "/species/search?" <> query_str
+    http_module.get(request_url, [timeout: timeout_milliseconds])
+  end
+
+  defp api_url do
+    "http://api.gbif.org/v1"
+  end
+
+  defp search_params do
+    %{limit: 50, nameType: "SCIENTIFIC", rank: "SPECIES", status: "ACCEPTED"}
+  end
+
+  defp timeout_milliseconds do
+    10_000
+  end
+end
